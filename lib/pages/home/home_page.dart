f967cfab98b751bb0f1be6a54db57b2e45c5482f
idikim <hjkim9217@naver.com>
2025-01-01 17:58:08 +0900

    feat: 가로 스크롤 기능 추가

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,22 +4,38 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
+      body: ListView(
+        children: [
+          Padding(
             padding: const EdgeInsets.all(20),
+            child: Text('가장 인기있는'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Expanded(
+                child: Image.network('https://picsum.photos/600/900',
+                    fit: BoxFit.cover)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text('현재 상영중'),
+          ),
+          Container(
+            height: 200,
             child: ListView(
+              scrollDirection: Axis.horizontal,
               children: [
-                Text('가장 인기있는'),
-                SizedBox(
-                  height: 20,
-                ),
-                Expanded(
-                    child: Image.network(
-                  'https://picsum.photos/600/900',
-                  fit: BoxFit.cover,
-                ))
+                ...List.generate(
+                  10,
+                  (index) => Container(
+                    width: 100,
+                    child: Image.network('https://picsum.photos/600/900'),
+                  ),
+                )
               ],
-            )),
+            ),
+          )
+        ],
       ),
     );
   }
