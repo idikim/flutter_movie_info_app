91e8b34c14720304c3dc91a43f71a8299f61656a
idikim <hjkim9217@naver.com>
2025-01-01 21:47:05 +0900

    refactor: homepage UI 완성

--- lib/pages/home/widgets/sort_by_popularity.dart
+++ lib/pages/home/widgets/sort_by_popularity.dart
@@ -8,35 +8,26 @@ class SortByPopularity extends StatelessWidget {
       SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           padding: EdgeInsets.only(left: 20),
-          child: Row(
-            children: [
-              ...List.generate(
+          child: Row(children: [
+            ...List.generate(
                 20,
-                (index) => Stack(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(left: 30),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network('https://picsum.photos/600/900',
-                            height: 180),
-                      ),
-                    ),
-                    Positioned(
-                        bottom: -20,
-                        child: Container(
-                          color: Colors.transparent,
-                          padding: EdgeInsets.only(right: 10),
-                          child: Text(
-                            '${index + 1}',
-                            style: TextStyle(fontSize: 100),
-                          ),
-                        ))
-                  ],
-                ),
-              ),
-            ],
-          ))
+                (index) => Stack(children: [
+                      Container(
+                          padding: EdgeInsets.only(left: 30),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                  'https://picsum.photos/600/900',
+                                  height: 180))),
+                      Positioned(
+                          bottom: -20,
+                          child: Container(
+                              color: Colors.transparent,
+                              padding: EdgeInsets.only(right: 10),
+                              child: Text('${index + 1}',
+                                  style: TextStyle(fontSize: 100))))
+                    ]))
+          ]))
     ]);
   }
 
